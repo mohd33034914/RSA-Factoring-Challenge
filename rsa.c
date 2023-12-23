aa48aada22e41793c14c6815d5005995bdeb1de1
mohd33034914 <mohd33034914@gmail.com>
2023-12-23 20:05:36 +0000

    rsa

--- rsa.c
+++ rsa.c
@@ -1,33 +1,58 @@
-#include "factor.h"
-
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
 /**
- * main - main function
+ * read_textfile- read and sent to stdout a number of charss.
  *
- * Author: Thaoban Abdrasheed
- * Return: void
+ *@filename: name of file
+ *@letters: number of letters
+ * Return: Number of printed chars.
  */
-int main(int argc, char *argv[])
+int main(int ac, char **av)
 {
-	FILE *fptr;
-	size_t count;
-	ssize_t line;
-	char *buffer = NULL;
+	int i, j, flag, fd, h;
+	int leido = 0;
+	long int number, number2, k;
+	char *buf, *buf2;
+	int letters = 100;
 
-
-	if (argc != 2)
+	leido = leido;
+	ac = ac;
+	number = 0;
+	fd = open(av[1], O_RDONLY);
+	buf = malloc(1000 * sizeof(char));
+	buf2 = malloc(120 * sizeof(char));
+	leido = read(fd, buf, letters);
+ 	i = 0;
+	while (buf[i] != 0)
 	{
-		fprintf(stderr, "Usage: factor <filename>\n");
-		exit(EXIT_FAILURE);
+		j = 0;
+		for (h = 0; h <= 100; h++)
+			buf2[h] = 0;
+		while (buf[i] != 10)
+			 {
+				 buf2[j] = buf[i];
+				 i++;
+				 j++;
+			 }
+		number = atol(buf2);
+		flag = 0;
+		for (k = 2; k < number /2 && flag == 0; k++)
+		{
+			if (number % k == 0)
+				flag = 1;
+		}
+		number2 = number / (k - 1);
+		printf("%li=%li*%li\n", number, number2, k - 1);
+		i++;
 	}
-	fptr = fopen(argv[1], "r");
-	if (fptr == NULL)
-	{
-		fprintf(stderr, "Error: can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
-	while((line = getline(&buffer, &count, fptr)) != -1)
-	{
-		factorize(buffer);
-	}
-return (0);
+	free(buf);
+	free(buf2);
+	close(fd);
+	return(0);
 }
